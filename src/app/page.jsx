7b682b8717226a502f71037e4d113f29ff00797c
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-05-15 08:26:21 -0300

    refactor: initial configs

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Games Retrô</h1>
+      <p>Recordes de jogos retrô</p>
+    </main>
+  );
+}
