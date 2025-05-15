a9cd53c270280d54937eae43075b84e8b6a458f6
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-05-15 08:45:04 -0300

    feat: create games

---
+++ src/app/games/page.jsx
@@ -0,0 +1,9 @@
+import styles from './games.module.css';
+
+export default function Games() {
+    return (
+        <div className={styles.games}>
+            <h1>Página de Games</h1>
+        </div>
+    )
+}
